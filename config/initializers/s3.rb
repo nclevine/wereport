dcf503eac582feaa52d229f3b6a767bfb6c115de
Noah Levine <nclevine@gmail.com>
2015-04-23 14:13:48 -0400

    changed some variable names

--- config/initializers/s3.rb
+++ config/initializers/s3.rb
@@ -1,8 +1,8 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
       :provider               => 'AWS',
-      :aws_access_key_id      => ENV['AWSAccessKeyId'],
-      :aws_secret_access_key  => ENV['AWSSecretKey']
+      :aws_access_key_id      => ENV['aws_access_key_id'],
+      :aws_secret_access_key  => ENV['aws_secret_access_key']
       # :region                 => ENV['S3_REGION'] # Change this for different AWS region. Default is 'us-east-1'
   }
   config.fog_directory  = ENV['bucket']
