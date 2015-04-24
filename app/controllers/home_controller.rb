8b04f10fbd15107328e41ff096fce624366147c7
Noah Levine <nclevine@gmail.com>
2015-04-24 12:11:58 -0400

    made trending stories 15 instead of 10

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def home
-    @trending_stories = Story.most_hit(1.day.ago, 10).sort_by {|story| story.importance}.reverse!
+    @trending_stories = Story.most_hit(1.day.ago, 15).sort_by {|story| story.importance}.reverse!
     @busy_neighborhoods = Neighborhood.all.sort_by {|neighborhood| neighborhood.stories.count}.reverse![0..4]
     @important_stories = Story.all.sort_by{|story| story.importance}.reverse![0..4]
     @most_viewed_stories = Story.most_hit
